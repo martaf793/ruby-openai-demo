17344dc516ed7e4599eef5c6837127f1c60a3175
martaf793 <martaf@uchicago.edu>
2025-04-09 23:39:25 +0000

    works

--- chat.rb
+++ chat.rb
@@ -6,43 +6,68 @@ client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
 
 
 # Prepare an Array of previous messages
-
-user_question=""
-while user_question!="bye"
-  message_list = [
+message_list = [
     {
       "role" => "system",
-      "content" => "Hello! How can I help you today?"
+      "content" => "You are a helpful assistant"
     }
   ]
-  pp message_list.at(0).fetch("content") 
-  #50.times do
-  #  pp "-"
-  #end
+user_question=""
+while user_question!="bye"
+    pp "Hello! How can I help you today?"
+    pp "-"*50
   user_question=gets.chomp
-  message_list.push(
-    {
-      "role" => "user",
-      "content" => user_question
-    }
-)
-# Call the API to get the next message from GPT
-  api_response = client.chat(
-    parameters: {
-      model: "gpt-3.5-turbo",
-      messages: message_list
-    }
-  )
+  if user_question != "bye"
+    message_list.push({ "role" => "user", "content" => user_question })
 
-  pp api_response.fetch("choices").at(0).fetch("message").fetch("content").chomp
-  #50.times do
-  #  pp "-"
-  #end
+    # Send the message list to the API
+    api_response = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: message_list
+      }
+    )
+
+    pp nice_reply=api_response.fetch("choices").at(0).fetch("message").fetch("content").chomp
+    pp "-" * 50
+
+    # Add the assistant's response to the message list
+    message_list.push({ "role" => "assistant", "content" => nice_reply })
+  end
 end
-pp api_response
-#next_message=api_response.fetch("choices").at(0).fetch("message")
-#pp next_message
 
-#message_list.push(next_message)
-#pp message_list
-#,{:role=> "user", :content =>"What about NYC?"}
+#HOW I DID UNTIL STEP 2. It woked the same. Understood that step3 was creating a "conversation" value, with all chat history.
+#require "openai"
+#require "dotenv/load"
+#client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+# Prepare an Array of previous messages
+#message_list = [
+#    {
+#      "role" => "system",
+#      "content" => "Hello! How can I help you today?"
+#    }
+#  ]
+#user_question=""
+#while user_question!="bye"
+#    pp message_list.at(0).fetch("content") 
+#    pp "-"*50
+#  user_question=gets.chomp
+#  
+#  message_list.push(
+#    {
+#      "role" => "user",
+#      "content" => user_question
+#    }
+#  )
+# Call the API to get the next message from GPT
+#  api_response = client.chat(
+#    parameters: {
+#      model: "gpt-3.5-turbo",
+#      messages: message_list
+#    }
+#  )
+#  pp api_response.fetch("choices").at(0).fetch("message").fetch("content").chomp
+#  pp "-"*50
+
+#next_message=api_response.fetch("choices").at(0).fetch("message")
+#pp conversation=message_list.push(next_message)
