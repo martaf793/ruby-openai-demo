5500be5014c4e50a2ea4c7bfb03c5c9ca0b32b85
martaf793 <martaf@uchicago.edu>
2025-04-09 17:05:15 +0000

    setting AI_gem

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source "https://rubygems.org"
+gem "dotenv"
+gem "ruby-openai"
--- chat.rb
+++ chat.rb
@@ -1,1 +1,8 @@
 # Write your solution here!
+pp "howdy, hello, Marta"
+require "openai"
+require "dotenv/load"
+
+client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+
+pp client
