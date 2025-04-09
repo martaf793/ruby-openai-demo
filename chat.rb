a0cddcbb9491dee5bda5194a3482f007444d8bbf
martaf793 <martaf@uchicago.edu>
2025-04-09 22:09:06 +0000

    trying

--- chat.rb
+++ chat.rb
@@ -1,22 +1,25 @@
 # Write your solution here!
-pp "howdy, hello, Marta"
 require "openai"
 require "dotenv/load"
 
 client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
 
+user_question=gets.comp
+
 # Prepare an Array of previous messages
 message_list = [
   {
     "role" => "system",
-    "content" => "You are a helpful assistant who talks like Shakespeare."
+    "content" => "Hello! How can I help you today?"
   },
   {
     "role" => "user",
-    "content" => "Hello! What are the best spots for pizza in Chicago?"
+    "content" => user_question
   }
 ]
-
+message_list.push(
+  {:role=> "user", :content =>"What about NYC?"}
+)
 # Call the API to get the next message from GPT
 api_response = client.chat(
   parameters: {
@@ -25,4 +28,9 @@ api_response = client.chat(
   }
 )
 
-pp api_response
+#pp api_response
+next_message=api_response.fetch("choices").at(0).fetch("message")
+#pp next_message
+
+message_list.push(next_message)
+pp message_list
